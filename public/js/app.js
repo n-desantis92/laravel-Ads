bad2e283a81d6fa8fe06ed70d0149c03c366f4e9
dsn <dsnwebdev@gmail.com>
2021-08-30 12:09:03 +0200

    add: setCookie
    aggiunta funzione che aggiunge il cookie al richiamo del metodo

--- public/js/app.js
+++ public/js/app.js
@@ -1888,10 +1888,39 @@ new Vue({
   data: {
     modale: true
   },
+  created: function created() {
+    this.setCookie('modulo', 'visitato');
+  },
+  mounted: function mounted() {},
   methods: {
     closeModal: function closeModal() {
       var contModal = document.getElementById('modale');
       contModal.style.display = 'none';
+    },
+    setCookie: function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    },
+    getCookie: function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+
+      return "";
     }
   }
 });
--- resources/js/modale.js
+++ resources/js/modale.js
@@ -2,11 +2,39 @@ new Vue({
     el: '#modale',
     data: {
         modale: true,
+    }, created() {
+        this.setCookie('modulo', 'visitato');
+    },
+    mounted() {
     },
     methods: {
         closeModal: function() {
             let contModal = document.getElementById('modale');
             contModal.style.display = 'none';
+
+        },
+
+        setCookie: function(cname, cvalue, exdays) {
+            const d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            let expires = "expires=" + d.toUTCString();
+            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        },
+        getCookie: function(cname) {
+            let name = cname + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
         }
+
     }
 })
